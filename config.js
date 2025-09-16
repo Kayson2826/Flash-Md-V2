3f1ad4dfa45e441f531cfbe67f2f4f74a5f0a080
Kayson2826 <saraharthur282612@gmail.com>
2025-09-16 09:10:35 +0000

    Kays Update config.js 2

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function mapPresence(val) {
         online: 'available',
         recording: 'recording',
         paused: 'paused',
-        offline: 'unavailable'
+        offline: 'available'
     };
     return mapping[val?.toLowerCase()?.trim()] || 'paused';
 }
@@ -16,7 +16,7 @@ module.exports = {
         ? process.env.PREFIX.split(',').map(p => p.trim())
         : [''],
 
-    NUMBER: process.env.YOUR_NUMBER || '254742063632',
+    NUMBER: process.env.YOUR_NUMBER || '233269805820',
     MODE: (process.env.MODE || 'private').toLowerCase().trim(),
     WARN_LIMIT: process.env.WARNINGS || '3',
     ON: process.env.YOUR_NAME || 'FLASH-MD',
@@ -26,7 +26,7 @@ module.exports = {
     AUTO_LIKE: process.env.AUTO_LIKE === 'on',
     AUTO_READ_MESSAGES: process.env.AUTO_READ_DM === 'on',
     HEROKU_API_KEY: process.env.HEROKU_API_KEY,
-    HEROKU_APP_NAME: process.env.HEROKU_APP_NAME,
+    HEROKU_APP_NAME: process.env.HEROKU_APP_NAME,kayson2826/
    ALIVE_URL: process.env.ALIVE_URL,
     sessionBase64: process.env.SESSION || '',
     timezone: 'Africa/Nairobi',
